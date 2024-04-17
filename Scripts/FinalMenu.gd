eacc50500c0beb648b707b748be5d834bfe8c999
bezark <jdb459@nyu.edu>
2024-04-17 16:09:23 -0400

    Add Mouse Mode to final scene

--- Scripts/FinalMenu.gd
+++ Scripts/FinalMenu.gd
@@ -9,7 +9,7 @@ func initialize(total_play_time : float) -> void:
 	var seconds : String = str(int(fmod(total_play_time, 60.0)))
 	var time_text = "Total time: %s m %s s" %[minutes, seconds]
 	time.text = time_text
-	
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	show()
 
 func _on_try_again_pressed():
